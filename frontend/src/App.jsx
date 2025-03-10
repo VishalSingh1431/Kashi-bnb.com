2405d7d9cddfca1be28ff2900620d25cd6ad3e28
VishalSingh1431 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-03-10 15:53:57 +0530

    started frnt & back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react' 
+import './App.css'
+import { bg_main } from './assets/Vars'
+
+function App() { 
+
+  return (
+    <>
+      <div className="sec tex">
+        sdlkfjsld
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/Vars.js
@@ -0,0 +1,6 @@
+const bg_main="#fefae0"
+const text_main="#283618"
+const bg_sec="#606c38"
+
+
+export {bg_main,bg_sec,text_main};
