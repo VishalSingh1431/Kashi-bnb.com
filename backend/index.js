8cbb1b71d3373bc76352625a129ea95681d9254c
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-10 21:00:41 +0530

    user routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import userRouter from './routes/user.js';
 
 const app = express();
 
-app.use('/api/v1/user');
-app.use('/api/v1/restr');
-app.use('/api/v1/hotel');
+app.use('/api/v1/user',userRouter);
+// app.use('/api/v1/restr');
+// app.use('/api/v1/hotel');
 
 app.listen(3000,()=>{
     console.log("app running on");
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
+import { loginControl, signupControl, checkControl } from "../controllers/user.js";
+import { authorisation } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get('/login');
-router.get('/signup');
+router.get('/login',loginControl);
+router.get('/signup',signupControl);
+router.get('/id/:uid',authorisation,signupControl);
+router.get('/check',checkControl);
 
 export default router;
