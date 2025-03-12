70538f2d248a5adb9cf8596e040b962d330e56a1
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-13 00:31:20 +0530

    mail

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ import express from 'express';
 import userRouter from './routes/user.js';
 
 const app = express();
+app.use(express.json());
 
 app.use('/api/v1/user',userRouter);
 // app.use('/api/v1/restr');
