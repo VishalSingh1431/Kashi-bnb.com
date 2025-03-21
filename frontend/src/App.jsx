cf4bd07007ebf739e3c9c8ebbeab4f4b2d2f6d54
VishalSingh1431 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-03-21 23:10:57 +0530

    SIgnup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ import { bg_main } from './assets/Vars'
 import Navbar from './Components/Navbar'
 import Slider from './Components/Slider'
 import Contact from './Pages/Contact' 
+import Signup from './Pages/signup'
 // https://themewagon.github.io/direngine/index.html
 function App() { 
 
   return (
     <>
+    
        <Navbar/>
+       <Signup/>
        <Slider/>
        <Contact/>
     </>
