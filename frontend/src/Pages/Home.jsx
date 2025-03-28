ca8b3e6346eba0678e861768783733eff4b7771a
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-29 00:04:55 +0530

    added success

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Slider from '../Components/Slider'
-import Login from './login'
+// import Login from './Login'
 import Signup from './Signup'
 import Contact from './Contact'
 import About from './About'
