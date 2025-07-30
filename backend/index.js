ac14dec979ee45857689462ec889a1d7f6143e12
VishalSingh1431 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-07-30 18:08:09 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -28,4 +28,4 @@ app.use('/api/v1/payments', paymentRouter);
 
 app.get('/', (req, res) => res.send("APIs for Kashi-BnB"));
 
-app.listen(443, () => console.log("Server running on port 443"));
+app.listen(5000, () => console.log("Server running on port 5000"));
