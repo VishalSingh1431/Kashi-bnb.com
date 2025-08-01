d157058367d59bbd83d0b42afedcb263bc0b7031
VishalSingh1431 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-08-01 11:18:48 +0530

    .

--- frontend/src/assets/Vars.js
+++ frontend/src/assets/Vars.js
@@ -2,8 +2,8 @@ const bg_main="#fefae0"
 const text_main="#283618"
 const bg_sec="#606c38"
 // export const BACKEND = "https://kashi-bnb-production.up.railway.app"
-export const BACKEND = "http://localhost:3000"
-// export const BACKEND = "https://kashibnb.com" // Use this for production
+// export const BACKEND = "http://localhost:3000"
+export const BACKEND = "https://kashibnb.com" // Use this for production
 
 export const RZPKID = "rzp_live_6WQjkxLwEAm4Wg";
 
