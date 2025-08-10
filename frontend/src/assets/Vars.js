5ef00a5ff86bb49e37d4fc94cab68481fa199be3
Vishahl99 <vishalkumar.singh.cer21@itbhu.ac.in>
2025-08-10 16:57:32 +0530

    .

--- frontend/src/assets/Vars.js
+++ frontend/src/assets/Vars.js
@@ -2,9 +2,9 @@ const bg_main="#fefae0"
 const text_main="#283618"
 const bg_sec="#606c38"
 // export const BACKEND = "https://kashi-bnb-production.up.railway.app"
-export const BACKEND = "http://localhost:3000"
+// export const BACKEND = "http://localhost:3000"
 // Use production backend URL
-// export const BACKEND = "https://backend.kashibnb.com";
+export const BACKEND = "https://backend.kashibnb.com";
 
 export const RZPKID = "rzp_live_6WQjkxLwEAm4Wg";
 
